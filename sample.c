4761cb9b1ffb781b0dfb26d947693a41dc28e703
nirajkalra <32154629+nirajkalra@users.noreply.github.com>
2017-09-29 19:51:46 +0200

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,9 @@
+// 'Hello World!' program ref https://en.wikibooks.org/wiki/C%2B%2B_Programming/Examples/Hello_world
+// to check language detection by git and contributions addition to the project
+#include <iostream>
+ 
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
